0952eb99ba3a91049b5c433223f947e102dbff7e
IshtiaqKhan <junk_FALSE@FAKE.com>
2013-04-04 21:38:02 +0100

    Revert "Updated version"
    This reverts commit d0dec4944e4cc4d04a70a296a0c996dae51376f9.

---
+++ Assets/Ish/Script/BossSpawner.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class BossSpawner : MonoBehaviour {
+	
+	
+public Material ad;
+	// Use this for initialization
+	void Start () {
+	  RenderSettings.skybox= ad;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
